cd2f5bf9443fcddb46969b4ad1e24277d23bb8bd
Specter-0 <sigurd.thornes@gmail.com>
2024-03-02 00:28:05 +0100

    gradient decent but working

--- ai.py
+++ ai.py
@@ -25,25 +25,34 @@ class Points():
 points = Points([Point(0.5, 1.4), Point(2.3, 1.9), Point(2.9, 3.2)])
 
 a1 = []
+i1 = []
 
 intercept = 0
-learning_rate = 0.1
+learning_rate = 0.01
 max_iterations = 1000
 while True:
     a1.append(sum(points.square_residuals(0.64, intercept)))
+    i1.append(intercept)
     
     step_size = sum(points.derivative_square_residuals(0.64, intercept)) * learning_rate
     print(step_size, sum(points.derivative_square_residuals(0.64, intercept)), intercept)
     
-    if abs(step_size) < 0.01 or max_iterations == 0: break
+    if abs(step_size) < 0.001 or max_iterations == 0: break
     
     intercept += -step_size
     max_iterations -= 1
 
+a2 = []
+i2 = []
+intercept = 0
+while intercept <= 2:
+    a2.append(sum(points.square_residuals(0.64, intercept)))
+    i2.append(intercept)
+    
+    intercept += 0.1
 
-plt.plot(a1, "ro")
-plt.plot(a1)
-
+plt.plot(i2, a2)
+plt.plot(i1, a1, "ro")
 plt.ylabel('sum of square residuals')
 plt.xlabel('intercept')
 plt.show()
