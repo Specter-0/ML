c5aa5c82fb5a3edfcd2e6594fd5ef5e79d1fbd58
Specter-0 <sigurd.thornes@gmail.com>
2024-03-01 23:16:57 +0100

    started on ml fr

---
+++ ai.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def residual(self, slope, intercept):
+        return self.y - (intercept + slope * self.x)
+        
+class Points():
+    def __init__(self, points : list[Point]) -> None:
+        self.points = points
+    
+    def residuals(self, slope, intercept):
+        return [point.residual(slope, intercept) for point in self.points]
+    
+    def square_residuals(self, residuals):
+        return [residual**2 for residual in residuals]
+
+points = Points([Point(0.5, 1.4), Point(2.3, 1.9), Point(2.9, 3.2)])
+
+tpp = []
+intercept = 0
+while intercept <= 2:
+    tpp.append(sum(points.square_residuals(points.residuals(0.64, intercept))))
+    intercept += 0.2
+
+plt.plot(tpp, "ro")
+plt.plot(tpp)
+plt.ylabel('sum of square residuals')
+plt.xlabel('intercept')
+plt.show()
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
 
 all:
 	python3 ./main.py $(if $(count), $(count), 1)
+
+ai:
+	python3 ./ai.py
