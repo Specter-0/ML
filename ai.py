c99d07e5e589688e1fe2ccb5b2e56e8e63491512
Specter-0 <sigurd.thornes@gmail.com>
2024-03-02 00:07:32 +0100

    gradient decent impl

--- ai.py
+++ ai.py
@@ -17,17 +17,33 @@ class Points():
     
     def square_residuals(self, slope, intercept):
         return [residual**2 for residual in self.residuals(slope, intercept)]
+    
+    def derivative_square_residuals(self, slope, intercept):
+        return [-2 * residual for residual in self.residuals(slope, intercept)]
+
 
 points = Points([Point(0.5, 1.4), Point(2.3, 1.9), Point(2.9, 3.2)])
 
-tpp = []
-intercept = 0
-while intercept <= 2:
-    tpp.append(sum(points.square_residuals(0.64, intercept)))
-    intercept += 0.2
+a1 = []
 
-plt.plot(tpp, "ro")
-plt.plot(tpp)
+intercept = 0
+learning_rate = 0.1
+max_iterations = 1000
+while True:
+    a1.append(sum(points.square_residuals(0.64, intercept)))
+    
+    step_size = sum(points.derivative_square_residuals(0.64, intercept)) * learning_rate
+    print(step_size, sum(points.derivative_square_residuals(0.64, intercept)), intercept)
+    
+    if abs(step_size) < 0.01 or max_iterations == 0: break
+    
+    intercept += -step_size
+    max_iterations -= 1
+
+
+plt.plot(a1, "ro")
+plt.plot(a1)
+
 plt.ylabel('sum of square residuals')
 plt.xlabel('intercept')
 plt.show()
