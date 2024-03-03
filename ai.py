b73a9dd958b52dfd882385e694117a379e42be71
Specter-0 <sigurd.thornes@gmail.com>
2024-03-03 15:55:57 +0100

    did backpropegation

--- ai.py
+++ ai.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
 import math, sys
 
-def bigboy(bias3value : float = 0) -> float:
-    sqr = lambda observed, predicted : (observed - predicted)**2
-
-    data = {0 : 0, 0.5 : 1, 1 : 0}
-
+def bigboy(bias3value : float, data : dict[float, float], do_draw = False) -> dict:
     w1 = lambda x : x * 3.32
     w2 = lambda x : x * -3.53
     w3 = lambda x : x * -1.22
@@ -18,11 +14,10 @@ def bigboy(bias3value : float = 0) -> float:
     top_f = lambda x : math.log(1 + math.exp(x)) # soft pluss (>_<)
     bottom_f = lambda x : math.log(1 + math.exp(x))
 
-    residuals = []
+    states = {}
     final_points = []
     x = 0
     while x <= 1:
-        x = round(x, 1)
         in_x = w1(x)
         in_x = b1(in_x)
         in_x = top_f(in_x)
@@ -35,29 +30,68 @@ def bigboy(bias3value : float = 0) -> float:
         
         final_points.append((x, b3(in_x + in_y)))
         if x in data.keys():
-            residuals.append(sqr(data[x], b3(in_x + in_y)))
+            states[x] = {
+                "predicted" : b3(in_x + in_y),
+                "observed" : data[x],
+            }
         
-        x += 0.1
+        x = round(x + 0.1, 1) # round to avoid floating point errors
 
-    #plt.plot(data.keys(), data.values(), "ro")
-    #plt.plot([point[0] for point in final_points], [point[1] for point in final_points], "b")
-    #plt.show()
+    if do_draw:
+        plt.plot(data.keys(), data.values(), "bo")
+        plt.plot([point[0] for point in final_points], [point[1] for point in final_points], "b")
+        plt.show()
 
-    #plt.plot(b3(0), sum(residuals), "ro")
-    #plt.show()
+    return states
+
+
+def lossfunc(states : list[dict], points : dict[float, float]) -> float:
+    ssqr = 0
+    dssqr = 0
+    for pointx, observed in points.items():
+        state = states[pointx]
+        
+        ssqr += (observed - state["predicted"]) **2
+        dssqr += (observed - state["predicted"]) * -2 
+        
+        
+    return (dssqr, ssqr)
     
-    return sum(residuals)
-
-arr = []
-arr2 = []
-x = 0
-while x <= 5:
-    print(bigboy(x))
+    learning_rate = 0.1
+    max_iterations = 10000
     
-    arr.append(x)
-    arr2.append(bigboy(x))
-    
-    x = round(x + 0.1, 1)
+    while True:
+        step_size = dssqr * learning_rate
+        
+        if abs(step_size) < 0.01 or max_iterations == 0: break
+        
+        max_iterations -= 1
+        
+data = {0 : 0, 0.5 : 1, 1 : 0}
 
-plt.plot(arr, arr2, "ro")
+xl = []
+yl = []
+learning_rate = 0.1
+max_iterations = 1000
+
+bias = 0
+while True:
+    states = bigboy(bias, data)
+    loss, ssqr = lossfunc(states, data)
+    
+    step_size = loss * learning_rate
+    if abs(step_size) < 0.001 or max_iterations == 0: break
+    
+    xl.append(bias)
+    yl.append(ssqr)
+    
+    bias += - step_size
+    max_iterations -= 1
+    
+    bigboy(bias, data, True)
+
+print("bias: ", bias)
+
+plt.plot(xl, yl)
 plt.show()
+
