33599851a51a2482783e0842d08fc47f310fb7e0
Specter-0 <sigurd.thornes@gmail.com>
2024-03-06 15:13:16 +0100

    idk what im doing

--- ai.py
+++ ai.py
@@ -2,323 +2,187 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from typing import Generator
 import numpy as np
-import math, sys
+import math, sys, time
 
 plt.style.use('fivethirtyeight')
 
 class NeuralNetwork():
     def __init__(self, points : dict[float, float]) -> None:
         self.points = points
-        
-        self.w1 = lambda x : x * np.random.normal(0, 1) 
-        self.w2 = lambda x : x * np.random.normal(0, 1) 
-        self.w3 = lambda x : x * np.random.normal(0, 1) 
-        self.w4 = lambda x : x * np.random.normal(0, 1) 
-        
-        self.b1 = lambda x : x + 0
-        self.b2 = lambda x : x + 0
-        self.b3 = lambda x : x + 0
-        
-        self.activation_function_1 = lambda x : math.log(1 + math.exp(x)) # soft pluss (>_<)
-        self.activation_function_2 = lambda x : math.log(1 + math.exp(x)) # soft pluss (>_<)
-    
-    def forward(self, x : float) -> float:
-        in_1 = self.w1(x)
-        in_1 = self.b1(in_1)
-        in_1 = self.activation_function_1(in_1)
-        in_1 = self.w3(in_1)
-        
-        in_2 = self.w2(x)
-        in_2 = self.b2(in_2)
-        in_2 = self.activation_function_2(in_2)
-        in_2 = self.w4(in_2)
-        
-        return self.b3(in_1 + in_2)
 
-    def halv_forward(self, x : float) -> tuple[float, float]:
-        in_1 = self.w1(x)
-        in_1 = self.b1(in_1)
-        in_1 = self.activation_function_1(in_1)
-        in_1 = self.w3(in_1)
+        # weight top before activation function
+        self.weight_tb = 3.32
+        self.bias_tb = -1.43
         
-        in_2 = self.w2(x)
-        in_2 = self.b2(in_2)
-        in_2 = self.activation_function_2(in_2)
-        in_2 = self.w4(in_2)
+        # weight bottom before activation function
+        self.weight_bb = -3.53
+        self.bias_bb = 0.57
         
-        return (in_1, in_2)
+        # weight top after activation function
+        self.weight_ta = -1.22
+        
+        # weight bottom after activation function
+        self.weight_ba = -2.30
+        
+        self.bias_sum = 2.60
+        
+    def __call__(self, x : float) -> float:
+        in_1 = self.weight_tb * x
+        in_1 = self.bias_tb + in_1
+        in_1 = self.soft_pluss(in_1)
+        in_1 = self.weight_ta * in_1
+        
+        in_2 = self.weight_bb * x
+        in_2 = self.bias_bb + in_2
+        in_2 = self.soft_pluss(in_2)
+        in_2 = self.weight_ba * in_2
+        
+        return self.bias_sum + (in_1 + in_2)
     
-    def traverse(self, minx, maxx, step):
-        return [x for x in np.arange(minx, maxx + step, step)], [self.forward(x) for x in np.arange(minx, maxx + step, step)]
+    def parts(self, x):
+        in_1 = self.weight_tb * x
+        xt = self.bias_tb + in_1
+        yt = self.soft_pluss(xt)
+        
+        in_2 = self.weight_bb * x
+        xb = self.bias_bb + in_2
+        yb = self.soft_pluss(xb)
+        
+        return (xt, xb, yt, yb)
     
-    def traverse_half(self, minx, maxx, step):
-        return [x for x in np.arange(minx, maxx + step, step)], [self.halv_forward(x) for x in np.arange(minx, maxx + step, step)]
-        
-    def __str__(self) -> str:
-        return f"""w1: {self.w1(1)} 
-w2: {round(self.w2(1), 3)} 
-w3: {round(self.w3(1), 3)} 
-w4: {round(self.w4(1), 3)} \n
-b1: {round(self.b1(1), 3)} 
-b2: {round(self.b2(1), 3)} 
-b3: {round(self.b3(1), 3)} 
-"""
-
-    def config(self) -> str:
-        return {
-            "w1" : round(self.w1(1), 3),
-            "w2" : round(self.w2(1), 3),
-            "w3" : round(self.w3(1), 3),
-            "w4" : round(self.w4(1), 3),
-            "b1" : round(self.b1(0), 3),
-            "b2" : round(self.b2(0), 3),
-            "b3" : round(self.b3(0), 3),
-        }
-
-class EzGraph():
-    def __init__(self, points : dict[float, float]) -> None:
-        self.chart : dict = {}
-        self.points = points
-        
-    def add(self, name):
-        fig, ax = plt.subplots()
-        line1 = ax.plot([], [], lw=2)[0]
-        line1.set_label("sum + bias3")
-        
-        line2 = ax.plot([], [], lw=2)[0]
-        line3 = ax.plot([], [], lw=2)[0]
-        
-        self.chart[name] = {"fig" : fig, "ax" : ax, "line1" : line1, "line2" : line2, "line3" : line3, "anim": None}
-        
+    def soft_pluss(self, x : float) -> float:
+        return math.log(1 + math.exp(x))
     
-    def animate(self, name : str, frames, interval : int, xlim : tuple[float, float] = None, ylim : tuple[float, float] = None):
-        def animate(state):
-            lx, lin_1, lin_2, ly  = state
+    def keypoints(self) -> Generator:
+        pindex = 0
+        for px, observed in self.points.items():
+            predicted = self(px)
+            x_top, x_bottom, y_top, y_bottom = self.parts(px)
             
-            self.chart[name]["line1"].set_data(lx, ly)
-            self.chart[name]["line2"].set_data(lx, lin_1)
-            self.chart[name]["line3"].set_data(lx, lin_2)
             
-            return self.chart[name]["line1"], self.chart[name]["line2"], self.chart[name]["line3"]
-        
-        def init():
-            if xlim: self.chart[name]["ax"].set_xlim(*xlim)
-            if ylim: self.chart[name]["ax"].set_ylim(*ylim)
+            yield ({
+                "point_index": pindex,
+                "point_x" : px,
+                "predicted" : predicted,
+                "observed" : observed,
+                "x_top" : x_top,
+                "x_bottom" : x_bottom,
+                "y_top": y_top,
+                "y_bottom": y_bottom,
+                "snapshot": {
+                    "weight_tb": self.weight_tb,
+                    "bias_tb": self.bias_tb,
+                    "weight_bb": self.weight_bb,
+                    "bias_bb": self.bias_bb,
+                    "weight_ta": self.weight_ta,
+                    "weight_ba": self.weight_ba,
+                    "bias_sum": self.bias_sum,
+                }
+            })
             
-            return self.chart[name]["line1"], self.chart[name]["line2"], self.chart[name]["line3"]
+            pindex += 1
+            
+    def train(self, *kwargs) -> None:
+        stuff = [(func, value, False) for func, value in kwargs]
         
-        self.chart[name]["anim"] = FuncAnimation(
-            self.chart[name]["fig"], 
-            animate, 
-            frames=frames, 
-            interval=interval, 
-            init_func=init, 
-            blit=True, 
-            repeat=False, 
-            cache_frame_data=False
-        )
+        while True:
+            states = list(self.keypoints())
+            
+            index = 0
+            for func, value, should_break in stuff:
+                print(value)
+                if not should_break:
+                    value, should_break = func(value, states)
+                    
+                    stuff[index] = (func, value, should_break)
+                
+                index += 1
+                    
+            if all([should_break for _, _, should_break in stuff]):
+                break
     
-    def plot_points(self):
-        plt.plot(self.points.keys(), self.points.values(), "bo")
+    def train_value(self, lossfunc, to_set, value, states) -> bool:
+        loss = 0
+        for state in states:
+            loss += lossfunc(state)
+            
+        value, should_break = self.gradient_descent(value, loss, 0.1, 0.00001)
         
-    def show(self):
-        plt.tight_layout()
-        plt.show()
-
-class Poly():
-    def __init__(self, xvalues, yvalues, degree) -> None:
-        self.xvalues = xvalues
-        self.yvalues = yvalues
-        self.degree = degree
+        match to_set:
+            case "tb":
+                self.bias_tb = value
+            
+            case "bb":
+                self.bias_bb = value
+            
+            case "sum":
+                self.bias_sum = value
+                
+            case "tb":
+                self.weight_tb = value
+            
+            case "bb":
+                self.weight_bb = value
+            
+            case "ta":
+                self.weight_ta = value
+            
+            case "ba":
+                self.weight_ba = value
         
-        self.coefficients = np.polyfit(self.xvalues, self.yvalues, self.degree)
+        return value, should_break
+            
     
-    def __call__(self, x):
-        return np.polyval(self.coefficients, x)
-
-    def traverse(self, minx, maxx, step):
-        return [x for x in np.arange(minx, maxx, step)], [self(x) for x in np.arange(minx, maxx, step)]
-    
-    def values(self, xvalues : list[float]):
-        return xvalues, [self(x) for x in xvalues]
-    
-class GradientDecent():
-    def __init__(self, learning_rate : float = 0.1, max_iterations : int = 1000, precision : float = 0.001) -> None:
-        self.learning_rate = learning_rate
-        self.precision = precision
-        self.max_iterations = max_iterations
-    
-    def __call__(self, value : float, loss : float) -> float:
-        step_size = loss * self.learning_rate
+    def gradient_descent(self, value, loss, learning_rate, precision) -> None:
+        step_size = loss * learning_rate
         
-        value += - step_size
-        self.max_iterations -= 1
-        return value, (abs(step_size) < self.precision or self.max_iterations == 0)
-
-
+        value += -step_size
+        
+        return value, abs(step_size) < precision
+        
 mynet = NeuralNetwork({0 : 0, 0.5 : 1, 1 : 0})
-gdw1 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdw2 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdw3 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdw4 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdb1 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdb2 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
-gdb3 = GradientDecent(max_iterations=1000, precision=0.0001, learning_rate=0.01)
 
-def step(optimized):
-    global mynet
-    lx, ly = mynet.traverse(0, 1, 0.1)
-    poly = Poly(lx, ly, 2)
-    
-    vb1 = None
-    vb2 = None
-    vb3 = None
-    
-    vw1 = None
-    vw2 = None
-    vw3 = None
-    vw4 = None
-    
-    if not optimized[0]:
-        loss = sum([(observed - predicted) * -2 for observed, predicted in zip(mynet.points.values(), poly.values(mynet.points.keys())[1])])
-        
-        vb3, should_break = gdb3(mynet.b3(0), loss)
-        
-        if should_break: optimized[0] = True
-        
-    if not optimized[1]: 
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_1 = mynet.w1(x)
-            in_1 = mynet.b1(in_1)
-            y1 = mynet.activation_function_1(in_1)
-            
-            loss += -2 * (observed - predicted) * y1
-        
-        vw3, should_break = gdw3(mynet.w3(1), loss)
-        
-        if should_break: optimized[1] = True
+#mynet.bias_sum = 0
+#mynet.weight_ta = np.random.normal(0, 1)
+#mynet.weight_ba = np.random.normal(0, 1)
+mynet.weight_tb = np.random.normal(0, 1)
 
-    if not optimized[2]:
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_2 = mynet.w2(x)
-            in_2 = mynet.b2(in_2)
-            y2 = mynet.activation_function_2(in_2)
-            
-            loss += -2 * (observed - predicted) * y2
-            
-        vw4, should_break = gdw4(mynet.w4(1), loss)
-        
-        if should_break: optimized[2] = True
+mynet.train(
+    #(lambda value, states : mynet.train_value(lambda state : -2 * (state["observed"] - state["predicted"]), "sum", value, states), mynet.bias_sum),
+    #(lambda value, states : mynet.train_value(lambda state : -2 * (state["observed"] - state["predicted"]) * state["y_top"], "ta", value, states), mynet.weight_ta),
+    #(lambda value, states : mynet.train_value(lambda state : -2 * (state["observed"] - state["predicted"]) * state["y_bottom"], "ba", value, states), mynet.weight_ba),
     
-    if not optimized[3]:
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_1 = mynet.w1(x)
-            x1 = mynet.b1(in_1)
-            
-            loss += -2 * (observed - predicted) * mynet.w3(1) * (math.exp(x1) / (1 + math.exp(x1))) * x
-            
-        vw1, should_break = gdw1(mynet.w1(1), loss)
-        
-        if should_break: optimized[3] = True
-        
-    if not optimized[4]:
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_1 = mynet.w1(x)
-            x1 = mynet.b1(in_1)
-            
-            loss += -2 * (observed - predicted) * mynet.w3(1) * (math.exp(x1) / (1 + math.exp(x1))) * 1
-            
-        vb1, should_break = gdb1(mynet.b1(0), loss)
-        
-        if should_break: optimized[4] = True
-    
-    if not optimized[5]:
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_2 = mynet.w2(x)
-            x2 = mynet.b2(in_2)
-            
-            loss += -2 * (observed - predicted) * mynet.w4(1) * (math.exp(x2) / (1 + math.exp(x2))) * x
-            
-        vw2, should_break = gdw2(mynet.w2(1), loss)
-    
-        if should_break: optimized[5] = True
-    
-    if not optimized[6]:
-        loss = 0
-        for x, observed in mynet.points.items():
-            predicted = poly(x)
-            
-            in_2 = mynet.w2(x)
-            x2 = mynet.b2(in_2)
-            
-            loss += -2 * (observed - predicted) * mynet.w4(1) * (math.exp(x2) / (1 + math.exp(x2))) * 1
-            
-        vb2, should_break = gdb2(mynet.b2(1), loss)
-    
-        if should_break: optimized[6] = True
-    
-    
-    if not optimized[3]: mynet.w1 = lambda x : x * vw1
-    if not optimized[4]: mynet.b1 = lambda x : x * vb1
-    
-    if not optimized[5]: mynet.w2 = lambda x : x * vw2
-    if not optimized[6]: mynet.b2 = lambda x : x * vb2
-    
-    if not optimized[1]: mynet.w3 = lambda x : x * vw3
-    if not optimized[2]: mynet.w4 = lambda x : x * vw4
-    if not optimized[0]: mynet.b3 = lambda x : x + vb3
-    
-    return optimized
+    (lambda value, states : mynet.train_value(lambda state : -2 * (state["observed"] - state["predicted"]) * state["snapshot"]["weight_ta"] * (np.exp(state["x_top"]) / (1 + np.exp(state["x_top"]))) * state["point_x"], "tb", value, states), mynet.weight_tb),
+)
 
-    
+lx = []
+ly = []
+ly1 = []
+ly2 = []
+for x in np.linspace(0, 1, 100, endpoint=True):
+    lx.append(x)
+    ly.append(mynet(x))
+    ly1.append(mynet.parts(x)[2] * mynet.weight_ta)
+    ly2.append(mynet.parts(x)[3] * mynet.weight_ba)
 
-def run() -> Generator[tuple[list[float], list[float]], None, None]:
-    global mynet
-    optimized = [False, False, False, False, False, False, False]
+plt.plot(lx, ly)
+plt.plot(lx, ly1)
+plt.plot(lx, ly2)
+plt.scatter(mynet.points.keys(), mynet.points.values(), c="r")
+plt.show()
 
-    while not all(optimized):
-        optimized = step(optimized)
-        
-        x, lxy = mynet.traverse_half(0, 1, 0.01)
-        lx, ly = zip(*lxy)
-        
-        yield (mynet.traverse(0, 1, 0.01), (x, lx, ly))
+print(f""" variables
 
-# shit is for drawing --------------------
+weight_tb: should be 3.32 is {mynet.weight_tb}
+bias_tb: should be -1.43 is {mynet.bias_tb}
 
-f, lin1_2 = zip(*list(run()))
+weight_bb: should be -3.53 is {mynet.weight_bb}
+bias_bb: should be 0.57 is {mynet.bias_bb}
 
-lx, in_1, in_2 = zip(*lin1_2)
+weight_ta: should be -1.22 is {mynet.weight_ta}
 
-ff = []
-for index, x in enumerate(lx):
-    ff.append((x, in_1[index], in_2[index], f[index][1]))
+weight_ba: should be -2.30 is {mynet.weight_ba}
 
+bias_sum: should be 2.60 is {mynet.bias_sum}
 
-gre = EzGraph(mynet.points)
-gre.add("main")
-gre.animate("main", frames=ff, interval=sys.argv[1], xlim=(-0.1, 1.1), ylim=(-3, 1.1))
-gre.plot_points()
-plt.legend()
-gre.show()
-
-
-# --------------------
-
-with open("values.txt", "w") as f:
-    f.write(str(mynet))
+""")
