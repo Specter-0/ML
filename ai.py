6405e1f922822c7d0147c93abcec2233ac74480f
Specter-0 <sigurd.thornes@gmail.com>
2024-03-08 15:03:59 +0100

    cool graph

--- ai.py
+++ ai.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Button, Slider
 import math, sys
 import numpy as np
 
@@ -15,10 +17,16 @@ class Nn:
         self.sbsum = 0.7
         
         self.pw3 = -0.1
+        self.pw4 = 2.4
+        self.pw5 = -2.2
         
         self.sw3 = 1.5
+        self.sw4 = -5.2
+        self.sw5 = 3.7
         
         self.bsum = 0
+        self.bsum2 = 0
+        self.bsum3 = 1
     
     def __call__(self, in1 : float, in2 : float) -> float:
         px = self.pw1 * in1
@@ -34,28 +42,22 @@ class Nn:
         py = self.ReLU(psum)
         sy = self.ReLU(ssum)
         
-        py = self.pw3 * py
-        sy = self.sw3 * sy
+        sp = self.pw3 * py
+        ss = self.sw3 * sy
         
-        sumed = py + sy + self.bsum
+        ssumed = sp + ss + self.bsum
         
-        return self.ReLU(sumed)
-    
-    def parts(self, in1 : float, in2 : float) -> float:
-        px = self.pw1 * in1
-        psx = self.sw1 * in2
+        vp = self.pw4 * py
+        vs = self.sw4 * sy
         
-        psum = (px + psx) + self.pbsum
+        vsumed = vp + vs + self.bsum2
         
-        sx = self.sw1 * in2
-        spx = self.pw2 * in1
+        vip = self.pw5 * py
+        vis = self.sw5 * sy
         
-        ssum = (sx + spx) + self.sbsum
+        visummed = vip + vis + self.bsum3
         
-        py = self.ReLU(psum)
-        sy = self.ReLU(ssum)
-        
-        return py, sy
+        return self.ReLU(ssumed), self.ReLU(vsumed), self.ReLU(visummed)
         
     
     def ReLU(self, x : float) -> float:
@@ -67,7 +69,7 @@ class Nn:
 mynet = Nn()
 
 fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
@@ -77,23 +79,54 @@ ax.set_xlabel("in1")
 ax.set_ylabel("in2")
 ax.set_zlabel("out")
 
-print(mynet(0, 0))
+ax.grid(False)
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_zticks([])
 
-lx = []
-ly = []
+ax.add_artist(ax.legend(["Setosa", "Versicolor", "Virgincia"]))
+
+axalp = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+alpha_slider = Slider(
+    ax=axalp,
+    label="Alpha",
+    valmin=0,
+    valmax=1,
+    valinit=0.85,
+    orientation="vertical"
+)
+
+
+lx2 = np.linspace(0, 1, 10)
+ly2 = np.linspace(0, 1, 10)
+lx, ly = np.meshgrid(lx2, ly2)
+
 lz = []
-lpz1 = []
-lpz2 = []
-for x in list(np.linspace(0, 1, 10)):
-    for y in list(np.linspace(0, 1, 10)):
-        lx.append(x)
-        ly.append(y)
-        lz.append(mynet(x, y))
-        p1, p2 = mynet.parts(x, y)
-        lpz1.append(p1)
-        lpz2.append(p2)
+lz2 = []
+lz3 = []
+for x in lx2:
+    for y in ly2:
+        y1, y2, y3 = mynet(x, y)
+        lz.append(y1)
+        lz2.append(y2)
+        lz3.append(y3)
 
-ax.scatter(lx, ly, lz, color="red", s=100)
-ax.scatter(lx, ly, lpz1, color="blue", s=100)
-ax.scatter(lx, ly, lpz2, color="yellow", s=100)
+# Convert lz and lz2 to 2D arrays
+lz = np.array(lz).reshape(lx.shape)
+lz2 = np.array(lz2).reshape(lx.shape)
+lz3 = np.array(lz3).reshape(lx.shape)
+
+surf1 = ax.plot_surface(lx, ly, lz, alpha=0.8, cmap="viridis")
+surf2 = ax.plot_surface(lx, ly, lz2, alpha=0.8, cmap="plasma")
+surf3 = ax.plot_surface(lx, ly, lz3, alpha=0.8, cmap="inferno")
+
+def update(val):
+    alpha = alpha_slider.val
+    surf1.set_alpha(alpha)
+    surf2.set_alpha(alpha)
+    surf3.set_alpha(alpha)
+    fig.canvas.draw_idle()
+
+alpha_slider.on_changed(update)
+
 plt.show()
